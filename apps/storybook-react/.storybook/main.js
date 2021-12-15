216e9c51c0b30a80a0e69f432640357824ede10b
Moonshot Factory <71419620+HappyCodingLover@users.noreply.github.com>
2021-12-15 16:42:58 -0500

    Text component added to storybook-react

--- apps/storybook-react/.storybook/main.js
+++ apps/storybook-react/.storybook/main.js
@@ -4,6 +4,7 @@ module.exports = {
   stories: [
     '../stories/**/*.stories.mdx',
     '../stories/**/*.stories.@(js|jsx|ts|tsx)',
+    '../stories/**/**/**/*.stories.@(js|jsx|ts|tsx)',
     '../../../packages/app/**/*.stories.@(js|jsx|ts|tsx)',
     '../../../packages/app/**/**/*.stories.@(js|jsx|ts|tsx)',
     '../../../packages/app/**/**/**/.stories.@(js|jsx|ts|tsx)'
